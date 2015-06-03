2cfd13447b503c1f8254918369b149eb92fa8392
Lyle Franklin <lylejfranklin@gmail.com>
2015-06-02 22:34:10 -0700

    Set default git push behavior

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -9,3 +9,8 @@ execute "set git email" do
   command "git config --global user.email \"#{node["git"]["email"]}\""
   user node['my_user']
 end
+
+execute "set default push behavior" do
+  command "git config --global push.default simple"
+  user node['my_user']
+end
