e83796a3e2cf93c22315fdf3096f2d56ad9fc19f
Lyle Franklin <lylejfranklin@gmail.com>
2015-06-03 20:21:54 -0700

    Add spotify installation

---
+++ cookbooks/spotify/recipes/default.rb
@@ -0,0 +1,20 @@
+apt_repository "google-chrome" do
+  uri "http://repository.spotify.com"
+  components ["stable", "non-free"]
+  keyserver 'hkp://keyserver.ubuntu.com:80'
+  key '94558F59'
+  action :add
+end
+
+# Temporary fix until spotify updates to current version of libgcrypt
+remote_file "/tmp/libgcrypt11.deb" do
+  source "http://security.ubuntu.com/ubuntu/pool/main/libg/libgcrypt11/libgcrypt11_1.5.4-2ubuntu1.1_amd64.deb"
+  mode 0644
+end
+
+dpkg_package "libgcrypt11" do
+  source "/tmp/libgcrypt11.deb"
+  action :install
+end
+
+package 'spotify-client'
