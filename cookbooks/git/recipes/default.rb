85ce531a74d0286c32ba39f96b0a3887a5659e5c
Lyle Franklin <lylejfranklin@gmail.com>
2015-06-03 20:40:52 -0700

    Add git aliases

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -14,3 +14,16 @@ execute "set default push behavior" do
   command "git config --global push.default simple"
   user node['my_user']
 end
+
+bash "set git aliases" do
+  user node['my_user']
+  code <<-EOH
+  git config --global alias.co checkout
+  git config --global alias.br branch
+  git config --global alias.ci commit
+  git config --global alias.st status
+  git config --global alias.unstage 'reset HEAD --'
+  git config --global alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit --all"
+  git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+  EOH
+end
