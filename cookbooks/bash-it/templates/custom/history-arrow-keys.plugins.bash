0f4b96b41592136c78bce6a522d1a468ad54206c
Lyle Franklin <lylejfranklin@gmail.com>
2015-06-13 09:01:53 -0700

    Only run keybinding in interactive terminal

--- cookbooks/bash-it/templates/custom/history-arrow-keys.plugins.bash
+++ cookbooks/bash-it/templates/custom/history-arrow-keys.plugins.bash
@@ -1,5 +1,8 @@
 cite about-plugin
 about-plugin 'Search backwards and fowards in history with arrow keys'
 
-bind '"\e[A": history-search-backward'
-bind '"\e[B": history-search-forward'
+# If running in interactive terminal
+if [ -t 1 ]; then
+  bind '"\e[A": history-search-backward'
+  bind '"\e[B": history-search-forward'
+fi
