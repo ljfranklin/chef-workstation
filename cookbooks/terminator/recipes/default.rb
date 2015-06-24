6668dcba40130c3e99fb86a8520a48a080ad6e5a
Lyle Franklin <lylejfranklin@gmail.com>
2015-06-23 22:22:43 -0700

    Add terminator config directory

--- cookbooks/terminator/recipes/default.rb
+++ cookbooks/terminator/recipes/default.rb
@@ -6,6 +6,12 @@ end
 
 package 'terminator'
 
+directory "/home/#{node['my_user']}/.config/terminator" do
+  owner node['my_user']
+  mode '0755'
+  action :create
+end
+
 template "/home/#{node['my_user']}/.config/terminator/config" do
   source 'config'
   owner node['my_user']
