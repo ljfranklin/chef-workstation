cfc437db9cda88e34767bcbca8bf019237a54c65
Lyle Franklin <lylejfranklin@gmail.com>
2015-10-24 11:16:51 -0700

    Install hal flash plugin to fix Hulu playback

--- cookbooks/firefox/recipes/default.rb
+++ cookbooks/firefox/recipes/default.rb
@@ -1,1 +1,10 @@
 package 'firefox'
+
+# Temporary fix until hulu ditches flash
+apt_repository "hal-flash" do
+  uri "ppa:flexiondotorg/hal-flash"
+  distribution node['lsb']['codename']
+  action :add
+end
+
+package "libhal1-flash"
