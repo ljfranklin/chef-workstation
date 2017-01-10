0e427b2e3693e6cc081355601d670570b60090a2
Lyle Franklin <lylejfranklin@gmail.com>
2017-01-10 13:32:39 -0800

    Add comment to document 16.10 changes

--- cookbooks/trackpad/recipes/default.rb
+++ cookbooks/trackpad/recipes/default.rb
@@ -1,5 +1,6 @@
 package 'xserver-xorg-input-libinput'
 
+# a forum indicated this file was renamed to 60-libinput.conf in Ubuntu 16.10
 template '/usr/share/X11/xorg.conf.d/90-libinput.conf' do
   source '90-libinput.conf'
   mode '0755'
