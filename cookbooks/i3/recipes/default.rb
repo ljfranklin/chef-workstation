98ddf37e195dceb641eff19e687aa9f1c785157e
Lyle Franklin <lylejfranklin@gmail.com>
2017-12-26 13:06:25 -0800

    Add greenclip as clipboard manager

--- cookbooks/i3/recipes/default.rb
+++ cookbooks/i3/recipes/default.rb
@@ -1,6 +1,7 @@
 case node['platform']
 when 'arch'
   package 'rofi'
+  pacman_aur 'greenclip'
   pacman_group 'i3' do
     action :install
   end
@@ -42,4 +43,11 @@ template "#{ENV['HOME']}/.config/i3status/config" do
   mode '0755'
   owner ENV['SUDO_USER']
   group ENV['SUDO_USER']
+end
+
+template '/etc/X11/xinit/xinitrc.d/60-greenclip.sh' do
+  source '60-greenclip.sh.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
 end
